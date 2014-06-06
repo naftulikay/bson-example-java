ea0a1e8471bf818d4df1381a00b1407088ab563f
Naftuli Tzvi Kay <rfkrocktk@gmail.com>
2014-06-06 11:08:10 -0700

    Still bugs. Strange, I can read out the full 12 bytes using readEmbeddedObject, but this breaks the next step in the chain.

--- src/main/java/org/tkassembled/example/bson/deserialize/BinaryObjectIdDeserializer.java
+++ src/main/java/org/tkassembled/example/bson/deserialize/BinaryObjectIdDeserializer.java
@@ -1,8 +1,12 @@
 package org.tkassembled.example.bson.deserialize;
 
+import static org.apache.commons.codec.binary.Hex.encodeHexString;
+
 import java.io.IOException;
 
 import org.bson.types.ObjectId;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -13,17 +17,20 @@ public class BinaryObjectIdDeserializer extends StdDeserializer<ObjectId> {
 	
 	private static final long serialVersionUID = 2075860824222316250L;
 
+	private static final Logger logger = LoggerFactory.getLogger(BinaryObjectIdDeserializer.class);
+	
 	public BinaryObjectIdDeserializer() {
 		super(ObjectId.class);
 	}
 	
 	@Override
-	@SuppressWarnings("unused")
 	public ObjectId deserialize(JsonParser jp, DeserializationContext ctxt)
 			throws IOException, JsonProcessingException {
-		byte[] value = jp.getBinaryValue();
-		int valueSize = value.length;
-		boolean valid = ObjectId.isValid(new String(value));
+//		jp.read
+		byte[] value = (byte[]) jp.getBinaryValue();
+		
+		logger.debug("Read {}-byte ObjectId: {}; hex: {}", value.length, value, 
+				encodeHexString(value));
 		
 		return new ObjectId(value);
 	}
--- src/main/java/org/tkassembled/example/bson/serialize/BinaryObjectIdSerializer.java
+++ src/main/java/org/tkassembled/example/bson/serialize/BinaryObjectIdSerializer.java
@@ -1,8 +1,12 @@
 package org.tkassembled.example.bson.serialize;
 
+import static org.apache.commons.codec.binary.Hex.encodeHexString;
+
 import java.io.IOException;
 
 import org.bson.types.ObjectId;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.fasterxml.jackson.core.JsonGenerationException;
 import com.fasterxml.jackson.core.JsonGenerator;
@@ -11,19 +15,21 @@ import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 
 public class BinaryObjectIdSerializer extends StdSerializer<ObjectId> {
 	
+	private static final Logger logger = LoggerFactory.getLogger(BinaryObjectIdSerializer.class);
+	
 	public BinaryObjectIdSerializer() {
 		super(ObjectId.class);
 	}
 	
 	@Override
-	@SuppressWarnings("unused")
 	public void serialize(ObjectId value, JsonGenerator jgen,
 			SerializerProvider provider) throws IOException,
 			JsonGenerationException {
 		byte[] bytes = value.toByteArray();
-		int byteSize = bytes.length;
-		byte[] stringBytes = value.toHexString().getBytes();
-		int stringBytesSize = stringBytes.length;
+		
+		logger.debug("Writing {}-byte binary ObjectId: {}, hex: {}", bytes.length, bytes,
+				encodeHexString(bytes));
+		
 		jgen.writeBinary(bytes);
 	}
 }
